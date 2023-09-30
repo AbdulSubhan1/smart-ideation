5661c2a822db9608788c3da734f688df32bda417
developeraccess786 <subbidurrani@gmail.com>
2023-10-01 02:01:02 +0500

    initial

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.scss";
+// import "bootstrap/scss/bootstrap.scss";
+import "./styles/scss/themes.scss";
+// import "./styles/scss/style.scss";
+import "./styles/css/materialdesignicons.min.css";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
